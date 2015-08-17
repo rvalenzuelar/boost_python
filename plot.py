141fc6179647fbcd3cdb0038a05c6b207bf76e48
rvalenzuelar <raul.valenzuela@colorado.edu>
2015-08-17 14:51:06 -0600

    correct orientation and using mask

--- plot.py
+++ plot.py
@@ -7,27 +7,26 @@ import numpy as np
 swp='swp.1010123212512.TA43P3.540.-19.5_AIR_v58'
 foo=Dorade.Dorade(swp)
 
+""" get dimensions """
 nrays=foo.getNumRays()
 ngates=foo.getNumGates()
 
+""" create 2D array """
 dbz=[]
 for i in range(nrays):
         dbz.append(foo.getReflectivity(i))
-
 DBZ=ma.asarray(dbz)
+DBZm=ma.masked_values(DBZ,-32768.0)
 
+""" grid for polar data """
 rays=np.linspace(0,2*np.pi,nrays)
 gates=np.linspace(0,45,ngates)
-r,theta = np.meshgrid(gates,rays) #rectangular plot of polar data
+r,theta = np.meshgrid(gates,rays) 
+r2x=r*np.sin(theta)*-1
+r2y=r*np.cos(theta)*-1
 
-r2x=r*np.sin(theta)
-r2y=r*np.cos(theta)
-
-# fig,ax=plt.subplots()
-# ax.imshow(DBZ,interpolation='none')
-# plt.show()
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.pcolormesh(r2x, r2y, DBZ) #X,Y & data2D must all be same dimensions
+ax.pcolormesh(r2x, r2y, DBZm) #X,Y & data2D must all be same dimensions
 plt.show()
